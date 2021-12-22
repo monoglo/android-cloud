5b34361cecb1452ab979d04d9795a13295c8804f
yr <monoglo37@gmail.com>
2021-12-22 18:06:36 +0800

    feat(deeplink): add deep link

--- android/app/src/main/java/com/dgtec/androidcloud/MainActivity.java
+++ android/app/src/main/java/com/dgtec/androidcloud/MainActivity.java
@@ -1,5 +1,21 @@
 package com.dgtec.androidcloud;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            Intent in = getIntent();
+            Uri data = in.getData();
+            System.out.println("deeplinkingcallback   :- "+data);
+        } catch (Exception ignored) {
+
+        }
+    }
+}
